6c90c2bb24d40d38bd03a45f7e24ab6fe21a46e6
杨黎祚 <1242781976@qq.com>
2020-09-22 09:17:35 +0800

    SSAS

---
+++ demo1/WKN/First.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class First : MonoBehaviour {
+
+    private Transform camTrans;
+    private Vector3 camAng;
+    private float camHeight = 0f;
+   
+	// Use this for initialization
+	void Start () {
+        camTrans = GetComponent<Transform>();
+        Vector3 startPos = transform.position;
+        startPos.y += camHeight;
+       
+        camTrans.position = startPos;
+        camTrans.rotation = transform.rotation;
+        camAng = camTrans.eulerAngles;
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float y = Input.GetAxis("Mouse X");
+        float x = Input.GetAxis("Mouse Y");
+        camAng.x -= x;
+        camAng.y += y;
+        camTrans.eulerAngles = camAng;
+        camTrans.position = new Vector3(this.transform.position.x, camTrans.position.y, this.transform.position.z);
+        float camy = camAng.y;
+        this.transform.eulerAngles = new Vector3(this.transform.eulerAngles.x, camy, this.transform.eulerAngles.z);
+        Vector3 startPos = transform.position;
+        startPos.y += camHeight;
+        
+        camTrans.position = startPos;
+    }
+}
---
+++ demo1/WKN/MonsterMove.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonsterMove : MonoBehaviour
+{
+
+    private Animator a;
+    public float m_speed = 5f;
+    public GameObject player;
+   
+
+    // Use this for initialization
+    void Start()
+    {
+        a = GetComponent<Animator>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            a.SetBool("Walk", true);
+            this.transform.Translate(Vector3.forward * m_speed * Time.deltaTime);
+        }   
+        else if (Input.GetKey(KeyCode.S))
+        {
+            a.SetBool("Walk", false);
+            this.transform.Translate(Vector3.forward * -m_speed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            a.SetBool("Walk", false);
+            this.transform.Translate(Vector3.right * -m_speed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            a.SetBool("Walk", false);
+            this.transform.Translate(Vector3.left * -m_speed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.LeftShift))
+        {
+            a.SetBool("Run", true);
+        }
+        else if (Input.GetKey(KeyCode.Space))
+        {
+            this.transform.Translate(Vector3.up * 4f * Time.deltaTime);
+            a.SetTrigger("Jump");
+            this.transform.Translate(Vector3.down * 4f * Time.deltaTime);
+        }
+        if (Input.GetMouseButton(0))
+        {
+            a.SetTrigger("Attack");
+        }
+
+
+    }
+}
